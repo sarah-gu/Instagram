08cceee602d89beca679eac6bf4a346a5da9fae7
Sarah Gu <guwen888@gmail.com>
2021-07-06 10:47:25 -0400

    Initial Commit

---
+++ Instagram/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Instagram
+//
+//  Created by Sarah Wen Gu on 7/6/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
